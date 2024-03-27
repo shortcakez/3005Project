e3ba3dd5b7a01efeef2f3c326246ada9df8ae1a5
lucy 3 <lucywang0408@gmail.com>
2024-03-27 19:21:01 -0400

    updated display menu options

--- application.py
+++ application.py
@@ -29,10 +29,7 @@ def login():
 
     while loggedin == False:
         #which type of member are they
-        memType = input(">> ")
-        while int(memType) < 1 or int(memType) > 6:
-            print("invalid option, try again")
-            memType = input(">> ")
+        memType = validate(1, 6)
 
         #if they are a new member registering
         if memType == "4":
@@ -60,8 +57,8 @@ def match(memType, id):
         cursor.execute("SELECT id, password FROM trainers")
     else:
         cursor.execute("SELECT id, password FROM admins")
-
     dataset = cursor.fetchall()
+
     matched = False
     for data in dataset:
         if id == data[0]:
@@ -70,7 +67,7 @@ def match(memType, id):
     
     return matched
 
-#display menu
+#display menu and get user input for what they want to do
 def menu(memType):
     print("\nSelect one of the following options (# only):")
     if memType == 1: #display member options
@@ -79,35 +76,64 @@ def menu(memType):
         print("\t 2. Display Dashboard")
         print("\t 3. Manage Schedule")
         print("\t 4. Logout")
-        select = input(">> ")
+        select = validate(1, 4)
 
         #sub options
         if select == "1":
             print("\t 1. Update personal information")
             print("\t 2. Update fitness goals")
             print("\t 3. Update health metrics")
-            subSelect = input(">> ")
+            subSelect = validate(1, 3)
             return "update" + subSelect
         elif select == "2":
             print("\t 1. Display exercise routines")
             print("\t 2. Display fitness acheivements")
             print("\t 3. Display health statistics")
-            subSelect = input(">> ")
+            subSelect = validate(1, 3)
             return "display" + subSelect
         elif select == "3":
             print("\t 1. Schedule personal training session")
             print("\t 2. Schedule group fitness class")
-            subSelect = input(">> ")
+            subSelect = validate(1, 2)
             return "schedule" + subSelect
         elif select == "4":
             print("LOGGING OUT")
             return "0"
 
     elif memType == 2: #dipslay trainer options
-        print("")
-    else: #display admin options
-        print("")
+        #main options
+        print("\t 1. Manage Schedule")
+        print("\t 2. View Member Profile")
+        print("\t 3. Logout")
+        select = validate(1, 3)
 
+        if select == "3":
+            print("LOGGING OUT")
+            return "0"
+
+        return select
+    else: #display admin options
+        #main options
+        print("\t 1. Manage Room Bookings")
+        print("\t 2. Monitor Equipement Maintainence")
+        print("\t 3. Update Class Schedule")
+        print("\t 4. Process Billing and Payment")
+        print("\t 5. Logout")
+        select = validate(1, 5)
+
+        if select == "5":
+            print("LOGGING OUT")
+            return "0"
+
+        return select
+
+#ensures that the user input is a valid option, otherwise, it'll keep asking user for input
+def validate(min, max):
+    select = input(">> ")
+    while int(select) < min or int(select) > max:
+            print("invalid option, try again")
+            select = input(">> ")
+    return select
     
 #MEMEBER FUNCTIONS
 #add new user
@@ -132,26 +158,65 @@ def userRegistration():
 
 #APPLICATION SET UP
 #login user
-memType = login()
+memType = 1
+select = ""
 
 while memType != 0:
-    #display menu options
-    select = menu(memType)
-
-    #executing based off input
+    memType = login()
     while select != "0":
-        if memType == 1:
-            if select == "update1":
-                #function
-                print("")
-        elif memType == 2:
-            if select == "":
-                #function
-                print("")
-        elif memType == 3:
-            if select == "":
-                #function
-                print("")
+        #displaying menu options and getting user selection
         select = menu(memType)
-    
+
+        #logging out to login menu
+        if select == 0:
+            break
+
+        #allocating what function to call
+        if memType == 1: #calling member functions
+            if select == "update1":
+                #update personal info
+                print("")
+            elif select == "update2":
+                #update fitness goals
+                print("")
+            elif select == "update3":
+                #update health metrics
+                print("")
+            elif select == "display1":
+                #display exercise routines
+                print("")
+            elif select == "display2":
+                #display fitness achievements
+                print("")
+            elif select == "display3":
+                #display health stats
+                print("")
+            elif select == "schedule1":
+                #schedule personal training session
+                print("")
+            elif select == "schedule2":
+                #schedule group fitness class
+                print("")
+
+        elif memType == 2: #calling trainer functions
+            if select == "1":
+                #schedule management
+                print("")
+            elif select == "2":
+                #view member profile
+                print("")
+
+        elif memType == 3: #calling admin functions
+            if select == "1":
+                #manage room booking
+                print("")
+            elif select == "2":
+                #monitor eqipement
+                print("")
+            elif select == "3":
+                #update class schedule
+                print("") 
+            elif select == "4":
+                #process billing and payment
+                print("")
     
