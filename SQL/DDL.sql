3352a87d3f3bf550cac86db2b6802674eadf6c8c
vivianngan <99743770+vivianngan@users.noreply.github.com>
2024-03-30 13:20:46 -0400

    Update DDL.sql

---
+++ SQL/DDL.sql
@@ -0,0 +1,63 @@
+CREATE TABLE Members (
+    member_id SERIAL PRIMARY KEY,
+    fname VARCHAR(30) NOT NULL,
+    lname VARCHAR(30) NOT NULL,
+    age SMALLINT NOT NULL,
+    weight NUMERIC(3,1),
+    height NUMERIC(3,1),
+    last_payment_date DATE,
+    next_payment_date DATE,
+    next_payment_amnt NUMERIC(3,2)
+);
+
+CREATE TABLE Fitness_goals (
+    goal_id INT,
+    member_id INT,
+    goal VARCHAR(255) NOT NULL,
+    achieved BOOLEAN,
+    PRIMARY KEY (member_id, goal_id),
+    FOREIGN KEY (member_id) REFERENCES Members (member_id)
+);
+
+CREATE TABLE Routine (
+    step INT NOT NULL,
+    member_id INT,
+    exercise VARCHAR(50) NOT NULL,
+    reps INT NOT NULL,
+    PRIMARY KEY (member_id, step),
+    FOREIGN KEY (member_id) REFERENCES Members (member_id)
+);
+
+CREATE TABLE Trainers (
+    trainer_id SERIAL PRIMARY KEY,
+    fname VARCHAR(30) NOT NULL,
+    lname VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Rooms (
+    room_id SERIAL PRIMARY KEY,
+    room_type VARCHAR(50)
+);
+
+CREATE TABLE Sessions (
+    session_id SERIAL PRIMARY KEY,
+    trainer_id INT,
+    room_num INT,
+    session_time TIME,
+    session_date DATE,
+    FOREIGN KEY (trainer_id)
+        REFERENCES Trainers (trainer_id),
+    FOREIGN KEY (room_num)
+        REFERENCES Rooms (room_id)
+);
+
+CREATE TABLE Admin_staff (
+    staff_id SERIAL PRIMARY KEY,
+    fname VARCHAR(30) NOT NULL,
+    lname VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Equipment_Maintenence (
+    equipment_id SERIAL PRIMARY KEY,
+    condition VARCHAR(30)
+);
